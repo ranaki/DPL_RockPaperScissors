535847e47464201688143670e5a87c6d6cde621f
rick baker <baker.rickw@gmail.com>
2017-02-01 23:40:04 -0700

    finished game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,13 @@
 $(document).ready(function(){
 
 var rpsArray = ['rock', 'paper','scissors'];
+var numMyWins = 0;
+var numComputerWins = 0;
+
+$('#reset').click( function() {
+    alert("This will reset the game");
+    $('#gameResults').empty();
+})
 
 $('#rock-btn').click(function(){
     compareRPS(randomRPS(), 'rock');
@@ -24,7 +31,54 @@ var randomRPS = function(){
 
 var compareRPS = function(computerRPS, myRPS) {
     // alert("Computer choice: " + computerRPS + "\nYour choice: " + myRPS);
-    $('#gameResults').append("<li>" + "Computer choice: " + computerRPS + "\nYour choice: " + myRPS + "</li>");
+    $('#gameResults').append("<li>Winner is: " + whoWins(computerRPS, myRPS) + " --- Computer choice: " + computerRPS + ", Your choice: " + myRPS + "</li>");
+    updateWinCount();
+}
+
+var whoWins = function(computerRPS, myRPS) {
+    //rock wins scissors
+    //paper wins rock
+    //scissors win paper
+    var winner = 'tie';
+
+    switch (computerRPS) {
+        case 'rock':
+            if (myRPS === 'scissors') {
+                winner = "computer";
+                numComputerWins ++;
+            } else if (myRPS === 'paper') {
+                winner = "you"
+                numMyWins ++;
+            }
+            break;
+        case 'paper':
+            if (myRPS === 'rock') {
+                winner = "computer";
+                numComputerWins ++;
+            } else if (myRPS === 'scissors') {
+                winner = "you"
+                numMyWins ++;
+            }
+            break;
+        case 'scissors':
+            if (myRPS === 'paper') {
+                winner = "computer";
+                numComputerWins ++;
+            } else if (myRPS === 'rock') {
+                winner = "you"
+                numMyWins ++;
+            }
+            break;
+    
+        default:
+            break;
+    }
+    return winner;
+}
+
+var updateWinCount = function() {
+    $('#myNumWins').text(numMyWins);
+    $('#compNumWins').text(numComputerWins);
 }
 
 }); // end document
