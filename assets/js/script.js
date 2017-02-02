bd8daeb5cdbeb430e7e453441977e459064ca16b
rick baker <baker.rickw@gmail.com>
2017-02-01 22:30:48 -0700

    add materialize and clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,20 @@ $(document).ready(function(){
 var rpsArray = ['rock', 'paper','scissors'];
 
 $('#rock-btn').click(function(){
-    // alert("You choose Rock");
     compareRPS(randomRPS(), 'rock');
-    // $('#gameResults').append("<li>" + "Rock" + "</li>");
 });
 
 $('#paper-btn').click(function() {
-    // alert("You choose Paper");
     compareRPS(randomRPS(), 'paper');
-    // $('#gameResults').append("<li>" + "Paper" + "</li>");
 });
 
 $('#scissors-btn').click(function() {
-    // alert("You choose Scisors");
     compareRPS(randomRPS(), 'scissors');
-    // $('#gameResults').append("<li>" + "Scissors" + "</li>");
 });
 
 
 var randomRPS = function(){
     var randomIndex = Math.floor(Math.random() * 3);
-    // alert("random number: " + randomIndex);
     return rpsArray[randomIndex];
 }
 
